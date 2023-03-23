705f5e994b190d56790498a37fe8104d3f7b8486
jaseel <jaseeltmc51@gmail.com>
2023-03-23 14:28:41 +0530

    olx home page completed

---
+++ src/Pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Banner from '../Components/Banner/Banner'
+import Footer from '../Components/Footer/Footer'
+import Header from '../Components/Header/Header'
+import Posts from '../Components/Posts/Posts'
+
+function Home() {
+  return (
+    <div>
+      <Header/>
+      <Banner/>
+      <Posts/>
+      <Footer/>
+     
+    </div>
+  )
+}
+
+export default Home
